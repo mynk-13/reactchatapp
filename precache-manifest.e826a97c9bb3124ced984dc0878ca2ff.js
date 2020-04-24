b12be2f9eaedb68039c50d5c90da3305274bb70c
Mayank Devrani <mayank.devrani13@gmail.com>
2020-04-24 21:02:38 +0530

    Updates

---
+++ precache-manifest.e826a97c9bb3124ced984dc0878ca2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3939881030d5551370aef2b492bdff",
+    "url": "/reactchatapp/index.html"
+  },
+  {
+    "revision": "71892b608d7f7f2f08d2",
+    "url": "/reactchatapp/static/css/main.fb545b00.chunk.css"
+  },
+  {
+    "revision": "c07fef59a512f5441331",
+    "url": "/reactchatapp/static/js/2.c2f1f4b0.chunk.js"
+  },
+  {
+    "revision": "fcdc5e536d87d4615bb78a0d5fa80be3",
+    "url": "/reactchatapp/static/js/2.c2f1f4b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71892b608d7f7f2f08d2",
+    "url": "/reactchatapp/static/js/main.c529928d.chunk.js"
+  },
+  {
+    "revision": "90ef5c61a879c198a1ee",
+    "url": "/reactchatapp/static/js/runtime-main.07653e71.js"
+  }
+]);
